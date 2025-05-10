db37cda8895249b1c0ccefaff286acbd86db3812
Sergey Sterkhov <129599465+ElecSir@users.noreply.github.com>
2025-05-10 18:11:33 +0300

    Update Prac3.cs

--- Prac3.cs
+++ Prac3.cs
@@ -1,3 +1,5 @@
+using System;
+
 public class Vector
 {
     public double Start { get; set; }
@@ -7,6 +9,16 @@ public class Vector
     {
         Start = start;
         End = end;
+    }
+
+    public double this[int index]
+    {
+        get
+        {
+            if (index == 0) return Start;
+            if (index == 1) return End;
+            throw new IndexOutOfRangeException("Индекс должен быть 0 или 1");
+        }
     }
 
     public static Vector operator +(Vector v1, Vector v2)
@@ -23,15 +35,41 @@ public class Vector
 
     public static bool operator ==(Vector v1, Vector v2)
     {
-        if (v1.Start == v2.Start && v1.End == v2.End) return true;
-        return false;
+        if (v1 is null || v2 is null) return false;
+        return v1.Start == v2.Start && v1.End == v2.End;
     }
 
     public static bool operator !=(Vector v1, Vector v2)
+        => !(v1 == v2);
+
+    public override bool Equals(object obj)
     {
-        if (v1.Start != v2.Start || v1.End != v2.End) return true;
-        return false;
+        return Equals(obj as Vector);
     }
 
-    public static void operator [](Vector )
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Start, End);
+    }
+
+    public override string ToString()
+    {
+        return $"({Start}, {End})";
+    }
+}
+
+public class Program
+{
+	public static void Main()
+	{
+		var v1 = new Vector(1, 2);
+		var v2 = new Vector(3, 4);
+
+		Console.WriteLine(v1 + v2);
+		Console.WriteLine(v1 - v2);
+		Console.WriteLine(v1 * 2);
+		Console.WriteLine(v1 * v2);
+		Console.WriteLine(v1[0]);
+		Console.WriteLine(v1 == v2);
+	}
 }
